008220cb5fa76fcf2e939d5021e2f050dbbc9448
cyxou <acyxou@gmail.com>
2017-12-01 15:32:54 +0300

    Add terminate function

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import {accountMgr}   from './lib/accountMgr';
 import {createLogger} from './lib/Logger';
 import * as playMacro from './lib/playMacro';
+import {terminate}    from './lib/terminate';
 
 export {
   createLogger,
   accountMgr,
-  playMacro
+  playMacro,
+  terminate
 };
